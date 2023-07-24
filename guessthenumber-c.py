3a520a9e672499f62aceb00b405d7fd60663c3bd
Sreeram P <psreeram3015@proton.me>
2023-07-24 20:38:11 +0530

    I forgot I haven't commented on any of the program. So I am doing it right now.

--- guessthenumber-c.py
+++ guessthenumber-c.py
@@ -1,8 +1,41 @@
 import random
+import sys
 
+# Input the lower and upper limits for the random number range
+lower = int(input("Enter the lower limit: "))
+upper = int(input("Enter the upper limit: "))
 
-def guess(limit):
-    random_num = random.randint(1, limit)
-    guess = 0
-    while guess != random_num:
-        limit
+# Generate a random number within the given range (inclusive)
+random_num = random.randint(lower, upper)
+
+# Inform the user to guess the random number and provide 8 chances to guess
+print("Now guess the number chosen by the system between", lower, "and", upper)
+print("You have 8 chances!")
+
+# Initialize the variable to keep track of the user's chances
+chances = 0
+
+# Start a loop to allow the user to make guesses
+for chances in range(8):
+    while True:
+        # Ask the user to enter their guess
+        guess = int(input("Enter your guess: "))
+
+        # Compare the user's guess with the random number
+        if random_num == guess:
+            print("Congratulations!🎉 You are correct. The number was", random_num)
+            sys.exit()
+        elif guess < random_num:
+            print("You guessed a smaller number. Try again.")
+        elif guess > random_num:
+            print("You guessed a larger number. Try again.")
+
+        # Check if the user has run out of chances
+        if chances == 7:
+            print("\nYou've run out of chances")
+            print("The number was", random_num)
+            print("Better luck next time.")
+            sys.exit()
+
+        print("\n")
+        break
--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,5 @@
 def spiderman_madlib():
+    # Get user input for various parts of the madlib story
     adj1 = input("Adjective: ")
     adj2 = input("Adjective: ")
     adj3 = input("Adjective: ")
@@ -15,6 +16,7 @@ def spiderman_madlib():
     spider_verb = input("Verb (related to spiders): ")
     place = input("Place: ")
 
+    # Construct the madlib story using the user-provided words
     madlib = f"Swinging through the {place} as {adj1} as a spider, {noun1}, also known as Spider-Man, \
 uses his {adj2} {noun2} to {verb} crime and protect the innocent. With his {adj3} spider-like senses, he can \
 detect danger from miles away, making him the {adj4} defender of {place}.\n\
@@ -26,7 +28,9 @@ The final showdown took place atop a towering {noun6}, and after an intense batt
 {verb_past2} the {noun3}, foiling his evil scheme and saving the day once again.\n\
 With great power comes great responsibility, and Spider-Man continues to be the {adj4} hero that inspires us all."
 
+    # Display the completed madlib story to the user
     print(madlib)
 
 if __name__ == '__main__':
+    # Execute the spiderman_madlib function when the script is run
     spiderman_madlib()
