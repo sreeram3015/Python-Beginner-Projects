3628748d620d5205c7a078af8bacf3c13edc3b00
Sreeram P <psreeram3015@proton.me>
2023-07-24 20:15:34 +0530

    Finished matlib. Will be going to the next project. But before that a new github repository have to be created and this file should be pushed into it.

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,32 @@
-adj = input("Adjective: ")
-verb1 = input("Verb1: ")
-verb2 = input("Verb2: ")
-famous_person = input("Famous Person: ")
+def spiderman_madlib():
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun6 = input("Noun: ")
+    verb = input("Verb: ")
+    verb_past = input("Verb (past tense): ")
+    verb_past2 = input("Verb (past tense): ")
+    spider_verb = input("Verb (related to spiders): ")
+    place = input("Place: ")
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+    madlib = f"Swinging through the {place} as {adj1} as a spider, {noun1}, also known as Spider-Man, \
+uses his {adj2} {noun2} to {verb} crime and protect the innocent. With his {adj3} spider-like senses, he can \
+detect danger from miles away, making him the {adj4} defender of {place}.\n\
+In one of his recent adventures, Spider-Man faced off against his arch-nemesis, {noun3}, a {noun4} \
+scientist turned evil. The {noun3}'s sinister plan involved using a powerful {noun5} to take control of \
+the city. As the city's safety hung in the balance, Spider-Man swung into action, {spider_verb} the walls \
+of buildings, and {verb_past} with incredible acrobatics.\n\
+The final showdown took place atop a towering {noun6}, and after an intense battle, Spider-Man \
+{verb_past2} the {noun3}, foiling his evil scheme and saving the day once again.\n\
+With great power comes great responsibility, and Spider-Man continues to be the {adj4} hero that inspires us all."
 
-print(madlib)    
+    print(madlib)
+
+if __name__ == '__main__':
+    spiderman_madlib()
