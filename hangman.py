f24be6f894025f595a8f892169e45b6ebed7c921
Sreeram P <psreeram3015@proton.me>
2023-07-25 12:59:00 +0530

    Defined a function get_valid_word

---
+++ hangman.py
@@ -0,0 +1,14 @@
+import random
+
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    
+    return word
+
+
