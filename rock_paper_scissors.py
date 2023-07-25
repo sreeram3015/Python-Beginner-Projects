afa866c1cace2d6d410d2a8788a191e90040e9ca
Sreeram P <psreeram3015@proton.me>
2023-07-25 11:14:51 +0530

    Used a while loop so that I can playthis game as many times as I want and made some minor changes.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,28 +3,40 @@ import sys
 
 
 def play():
-    user = input("Enter your choice \n\
-(Rock -> r, Paper -> p, Scissor -> s): \n").lower()
-
-    if (user != 'r') and (user != 'p') and (user != 's'):
-        print("Invalid input. You will automatically exit from this game!")
-        sys.exit()
+    # Initialize the variable 'choice' to '1' to enter the loop for the first time
+    choice = '1'
     
+    # Define the set of allowed user inputs (Rock -> 'r', Paper -> 'p', Scissor -> 's')
+    allowed_inputs = {'r', 'p', 's'}
     
-    computer = random.choice(['r', 'p', 's'])
+    # Start a loop to allow the user to play multiple rounds until they choose to stop
+    while choice == '1':
+        # Get the user's choice (Rock, Paper, or Scissor) and convert it to lowercase
+        user = input("Enter your choice (Rock -> r, Paper -> p, Scissor -> s): ").lower()
+        
+        # Check if the user's input is valid (in the allowed_inputs set)
+        if user not in allowed_inputs:
+            print("Invalid input! You will automatically exit from this game!")
+            sys.exit()
+        
+        # Generate a random choice for the computer (Rock, Paper, or Scissor)
+        computer = random.choice(['r', 'p', 's'])
 
-    if user == computer:
-        print(f"You and computer both chose {user}. So this is a tie!")
-    
-    if is_win(user,computer):
-        return f"You played {user} and computer played {computer}. So You won! 🎉"
+        # Compare the user's choice with the computer's choice to determine the result
+        if user == computer:
+            print(f"You and computer both chose {user}. So this is a tie!")
+        elif is_win(user, computer):
+            print(f"You played {user} and computer played {computer}. So You won! 🎉")
+        else:
+            print(f"You played {user} and computer played {computer}. So You lost! Better luck next time")
 
-    return f"You played {user} and computer played {computer}. So You lost! Better luck next time"
+        # Ask the user if they want to continue playing (Enter '1' to continue, any other key to stop)
+        choice = input("Do you want to continue playing? (Yes -> 1, No -> any other key): ")
 
 
 def is_win(user, computer):
-    if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p'):
-        return True
-    
+    # Check if the user's choice beats the computer's choice
+    return (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p')
 
-print(play())
+# Start the game by calling the play() function
+play()
