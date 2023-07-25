08d6f31685d2df62ada46049429baff947203e24
Sreeram P <psreeram3015@proton.me>
2023-07-25 13:14:35 +0530

    Finished basic structure of the game. Need a little bit of adjustments.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 from words import words
 
@@ -9,6 +10,31 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         word = random.choice(words)
     
-    return word
+    return word.upper()
 
 
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabets = set(string.ascii_uppercase)
+    used_letters = set()
+
+    while len(word_letters) > 0:
+        print("You've used these letter: ",' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print("Current word: ", ' '.join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+
+        if user_letter in alphabets - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print("You have already gussed this character!")
+
+        
+        else:
+            print("Invalid character!")
