35f229ebf3014614964037a6c143da2ec8066eb9
Sreeram P <psreeram3015@proton.me>
2023-08-01 14:40:14 +0530

    Second commit for player.py

--- player.py
+++ player.py
@@ -16,11 +16,26 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game. availabe_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+
+            try:
+                val = int(square)
+                if val not in game.availabe_moves():
+                    raise ValueError
+                
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again")
+        
+        return val
