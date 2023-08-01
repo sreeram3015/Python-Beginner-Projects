5d40e4cdec2e23c74d8799021b6c1bb527088f3c
Sreeram P <psreeram3015@proton.me>
2023-08-01 14:25:00 +0530

    Created a skeleton like structure in player.py

---
+++ player.py
@@ -0,0 +1,26 @@
+#This file is a part of Tic-Tac-Toe game
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
